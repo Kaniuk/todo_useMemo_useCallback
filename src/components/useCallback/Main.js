ab998b874ab264b6d0548279f41b784eced72ba4
Liudmyla <lyudmula1989@ukr.net>
2023-04-27 16:15:03 +0200

    todoList

--- src/components/useCallback/Main.js
+++ src/components/useCallback/Main.js
@@ -1,20 +1,44 @@
 import React, {useCallback, useState} from 'react';
-import ToDos from "./ToDos";
+
+import './Main.css';
 
 const Main = () => {
     const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState('');
     const [counter, setCounter] = useState(0);
 
     const inc = () => setCounter(prev => prev + 1);
-    // const addTodo = () => setTodos([...todos, 'New todo']);
 
-    const addTodo = useCallback(() => {
-        setTodos([...todos, 'New todo']);
-    }, [todos]);
+    const addTodos = () => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 1000),
+            value: todo
+        };
+
+        setTodos([...todos, newTodo]);
+        setTodo('');
+    };
+
+    function deleteTodo(id) {
+        const newArrTodos = todos.filter(removeTodo => removeTodo.id !== id);
+        setTodos(newArrTodos);
+    }
 
     return (
-        <div>
-            <ToDos todos={todos} addTodo={addTodo}/>
+        <div className="todoList">
+            <h1>TODO List</h1>
+            <input type="text"
+                   placeholder="add something"
+                   value={todo}
+                   onChange={e => setTodo(e.target.value)}/>
+            <button onClick={() => addTodos()}>add</button>
+
+            <div>
+                {todos?.map((todo) => <div key={todo.id} className="list">
+                    <div>{todo.value}</div>
+                    <button onClick={() => deleteTodo(todo.id)}>❌</button>
+                </div>)}
+            </div>
             <hr/>
             <h1>count:{counter}</h1>
             <button onClick={inc}>inc</button>
--- src/components/useCallback/ToDos.js
+++ src/components/useCallback/ToDos.js
@@ -1,13 +1,13 @@
-import React, {memo} from 'react';
-
-const ToDos = memo(({addTodo, todos}) => {
-    console.log('Todos child loading...');
-    return (
-        <div>
-            {todos.map((todo, index) => <p key={index}>{todo}</p>)}
-            <button onClick={addTodo}>add</button>
-        </div>
-    );
-});
-
-export default ToDos;
+// import React, {memo} from 'react';
+//
+// const ToDos = memo(({addTodo, todos}) => {
+//     console.log('Todos child loading...');
+//     return (
+//         <div>
+//             {todos.map((todo, index) => <p key={index}>{todo}</p>)}
+//
+//         </div>
+//     );
+// });
+//
+// export default ToDos;
